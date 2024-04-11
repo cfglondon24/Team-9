224380f5ed72aa7920f7bb65c58e7458b7767c20
morraconda <morra.conda@gmail.com>
2024-04-11 13:11:40 +0100

    Integrated chatbot onto blank website

---
+++ server.py
@@ -0,0 +1,9 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+test(CORSRequestHandler, HTTPServer, port=8000)
